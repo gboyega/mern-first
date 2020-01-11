f76e7359689f6d47e86c91e0c1fa23ecd667b557
gboyega <jaiyeolagboyega@gmail.com>
2020-01-11 09:34:06 +0100

    fixed build folder

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/api/", (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
