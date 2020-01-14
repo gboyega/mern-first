1af05cd3e89299be664651007750494a7c53ad0f
gboyega <jaiyeolagboyega@gmail.com>
2020-01-14 07:05:09 +0100

    deinit client submodule

---
+++ client/build/precache-manifest.e5bf755b30e9575f071e1029a16bea7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e246111699b55cc17aaeaf36e5b724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aca3e57670726300a9e0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4362a4f312606a7c4f0e",
+    "url": "/static/js/2.d85fda0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d85fda0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aca3e57670726300a9e0",
+    "url": "/static/js/main.bd068aa5.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
